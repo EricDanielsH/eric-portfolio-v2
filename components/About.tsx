4e3710c38d929c2d7d90842c05badfea1cd9d0e0
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-07-29 00:09:04 +0100

    Inital commit

---
+++ components/About.tsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <section className="container min-h-[60vh] px-8 bg-neutral-900 text-white max-w-2xl">
+      <h2
+        id="About"
+        className="text-xl md:text-4xl font-semibold text-white mb-4 tracking-tight animate-fade-in-slide-up delay-long"
+      >
+        About
+      </h2>
+      <p className="text-base md:text-lg text-neutral-200">
+        I am a software developer based in Lancaster, United Kingdom. I am passionate about
+        building software that solves real-world problems. I am a fast learner
+        and I am always looking for ways to improve my skills. I am proficient
+        in JavaScript and Python and I am currently learning Go.
+      </p>
+    </section>
+  );
+}
