a10cc741cf2e2c12f1d1d210633373df5d16da60
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-07-29 13:57:53 +0100

    Update about section

--- components/About.tsx
+++ components/About.tsx
@@ -1,17 +1,29 @@
 export default function About() {
   return (
-    <section id="about" className="container min-h-[60vh] px-8 bg-neutral-900 text-white max-w-2xl pt-[10vh]">
-      <h2
-        id="About"
-        className="text-xl md:text-4xl font-semibold text-white mb-4 tracking-tight animate-fade-in-slide-up delay-long"
-      >
-        About
+    <section
+      id="about"
+      className="container px-8 bg-neutral-900 text-white max-w-2xl pt-[10vh] mb-20"
+    >
+      <h2 className="text-xl md:text-4xl font-semibold text-white mb-4 tracking-tight animate-fade-in-slide-up delay-long">
+        About Me
       </h2>
-      <p className="text-base md:text-lg text-neutral-200">
-        I am a software developer based in Lancaster, United Kingdom. I am passionate about
-        building software that solves real-world problems. I am a fast learner
-        and I am always looking for ways to improve my skills. I am proficient
-        in JavaScript and Python and I am currently learning Go.
+      <p className="text-base md:text-lg text-neutral-300">
+        As a seasoned software developer based in Lancaster, United Kingdom, I
+        am deeply passionate about crafting innovative software solutions that
+        address real-world challenges. With a proven track record of{" "}
+        <strong className="text-white">rapid learning and adaptation</strong>, I thrive in dynamic
+        environments where innovation and excellence are paramount.
+        <br />
+        <br />
+        In my pursuit of knowledge, I am currently delving into the realms of{" "}
+        <strong className="text-white">
+          cybersecurity, blockchain technology, and quantum computing.
+        </strong>{" "}
+        These fields captivate my interest and fuel my ambition to
+        contribute to the future of technology in meaningful ways.
+        <br />
+        <br />
+        Beyond my professional endeavors, I am actively involved in the tech community. I participate in <strong className="text-white">conferences, hackathons, and meetups</strong>, where I enjoy sharing knowledge and learning from others. In my free time, I indulge in personal projects, exploring new programming paradigms and experimenting with the latest advancements in the field.
       </p>
     </section>
   );
