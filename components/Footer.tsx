4e27cbbb80c784060dfa0a31f019e5b2bde246ee
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-07-29 12:52:37 +0100

    Update contact

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,11 +2,12 @@
 import React from "react";
 import { BackgroundBeams } from "./ui/background-beams";
 import Image from "next/image";
+import Socials from "@/components/Socials";
 
 export default function Footer() {
   return (
     <footer className="mt-20 h-[20vh] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased overflow-hidden">
-      <div className="max-w-2xl mx-auto p-4 flex flex-col justify-center items-center relative">
+      <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4 justify-center items-center relative">
         <h3 className="relative text-lg md:text-2xl font-black text-center text-neutral-200 tracking-tighter z-10">
           Eric Daniels
         </h3>
@@ -16,11 +17,12 @@ export default function Footer() {
             width={400}
             height={400}
             alt="Eric Daniels"
-            className="opacity-30 w-[60%] md:w-[90%]" 
+            className="opacity-30 w-[60%] md:w-[90%]"
             style={{ objectFit: "contain" }}
           />
         </div>
-        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+        <Socials />
+        <p className="text-neutral-500 max-w-lg mx-auto text-sm text-center relative z-10">
           &copy; {new Date().getFullYear()} Eric Daniels. All rights reserved.
         </p>
       </div>
