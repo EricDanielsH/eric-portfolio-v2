6bf6c4df9bd13eb0acc9f223a0f98549d05559b7
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-10-01 19:29:11 +0100

    Add project and refactor code

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,52 @@
 import { ProjectCard } from "@/components/ProjectCard";
 
 export default function Projects() {
+  const projectData = [
+    {
+      title: "HybridCoach AI",
+      link: "https://hybridcoach-ai.vercel.app",
+      description:
+        "AI-powered fitness app that generates personalized workout plans based on user's goals, preferences, and fitness level.",
+      techStack: [
+        "Next.js",
+        "Typescript",
+        "OpenAI",
+        "Stripe",
+        "NextAuth",
+        "MongoDB",
+        "TailwindCss",
+      ],
+    },
+    {
+      title: "LUCribCheck",
+      link: "https://lucribcheck.com/",
+      description:
+        "Share and discover reviews on homes in Lancaster, UK, helping others make informed decisions about properties and neighborhoods.",
+      techStack: [
+        "Next.js",
+        "AWS S3",
+        "NextAuth",
+        "MongoDB",
+        "TailwindCss",
+        "Typescript",
+      ],
+    },
+    {
+      title: "HealthBuddy",
+      link: "https://drive.google.com/file/d/1UC5hHXO_k3-I8PoQ3owwLltYfy8i2MuS/view?usp=share_link",
+      description:
+        "Keep you and your Buddy healthy. Calorie tracking app that helps users maintain a healthy lifestyle.",
+      techStack: ["React Native", "Firebase"],
+    },
+    {
+      title: "Blockchain",
+      link: "https://github.com/EricDanielsH/go-blockchain",
+      description:
+        "A simple blockchain implementation in Go, developed using insights and techniques shared on my blog.",
+      techStack: ["Go", "BoltDB"],
+    },
+  ];
+
   return (
     <section
       id="projects"
@@ -10,38 +56,15 @@ export default function Projects() {
         Projects
       </h2>
       <div className="flex flex-col gap-8">
-        <ProjectCard
-          title="LUCribCheck"
-          link="https://lucribcheck.com/"
-          description={
-            "Share and discover reviews on homes in Lancaster, UK, helping others make informed decisions about properties and neighborhoods."
-          }
-          techStack={[
-            "Next.js",
-            "AWS S3",
-            "NextAuth",
-            "MongoDB",
-            "TailwindCss",
-            "Typescript",
-          ]}
-        />
-        <ProjectCard
-          title="HealthBuddy"
-          link="https://drive.google.com/file/d/1UC5hHXO_k3-I8PoQ3owwLltYfy8i2MuS/view?usp=share_link"
-          description={
-            "Keep you and your Buddy healthy. Calorie tracking app that helps users maintain a healthy lifestyle."
-          }
-          techStack={["React Native", "Firebase"]}
-        />
-
-        <ProjectCard
-          title="Blockchain"
-          link="https://github.com/EricDanielsH/go-blockchain"
-          description={
-            "A simple blockchain implementation in Go, developed using insights and techniques shared on my blog."
-          }
-          techStack={["Go", "BoltDB"]}
-        />
+        {projectData.map((project, index) => (
+          <ProjectCard
+            key={index}
+            title={project.title}
+            link={project.link}
+            description={project.description}
+            techStack={project.techStack}
+          />
+        ))}
       </div>
     </section>
   );
