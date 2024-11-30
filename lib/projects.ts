09348db4f81abb52870adcc20fdec00d3c89b86c
Eric Daniels <e.danielshuaman@lancaster.ac.uk>
2024-11-30 23:43:41 +0000

    Add a project

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,5 +1,21 @@
   export const projectData = [
     {
+      title: "SmartLab System",
+      link: "https://github.com/EricDanielsH/mc-eric-texture-pack",
+      description:
+        "System build for measuring and monitoring temperature, light sound and other environmental factors in a lab, using Raspberry Pi Pico Ws.",
+      techStack: [
+        "C",
+        "C++",
+        "Kotlin",
+        "React",
+        "Typescript",
+        "PlatfromIO",
+        "Raspberry Pi",
+        "PostgreSQL",
+      ],
+    },
+    {
       title: "Minecraft Texture Pack",
       link: "https://github.com/EricDanielsH/mc-eric-texture-pack",
       description:
