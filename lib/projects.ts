6a43682c81ae1057b0fd1f2676e8dae8529f9c2a
Eric Daniels <e.danielshuaman@lancaster.ac.uk>
2024-11-30 23:39:28 +0000

    Move project data into a file and export it

---
+++ lib/projects.ts
@@ -0,0 +1,58 @@
+  export const projectData = [
+    {
+      title: "Minecraft Texture Pack",
+      link: "https://github.com/EricDanielsH/mc-eric-texture-pack",
+      description:
+        "A Minecraft texture pack that adds a personal touch to the game, with custom textures and sounds.",
+      techStack: [
+        "Java",
+        "Minecraft",
+        "Photoshop",
+        "Paint.NET",
+      ],
+    },
+    {
+      title: "HybridCoach AI",
+      link: "https://hybridcoach-ai.vercel.app",
+      description:
+        "AI-powered fitness app that generates personalized workout plans based on user's goals, preferences, and fitness level.",
+      techStack: [
+        "Next.js",
+        "Typescript",
+        "OpenAI",
+        "Stripe",
+        "NextAuth",
+        "MongoDB",
+        "TailwindCss",
+      ],
+    },
+    {
+      title: "LUCribCheck",
+      link: "https://lucribcheck.com/",
+      description:
+        "Share and discover reviews on homes in Lancaster, UK, helping others make informed decisions about properties and neighborhoods.",
+      techStack: [
+        "Next.js",
+        "AWS S3",
+        "NextAuth",
+        "MongoDB",
+        "TailwindCss",
+        "Typescript",
+      ],
+    },
+    {
+      title: "HealthBuddy",
+      link: "https://drive.google.com/file/d/1UC5hHXO_k3-I8PoQ3owwLltYfy8i2MuS/view?usp=share_link",
+      description:
+        "Keep you and your Buddy healthy. Calorie tracking app that helps users maintain a healthy lifestyle.",
+      techStack: ["React Native", "Firebase"],
+    },
+    {
+      title: "Blockchain",
+      link: "https://github.com/EricDanielsH/go-blockchain",
+      description:
+        "A simple blockchain implementation in Go, developed using insights and techniques shared on my blog.",
+      techStack: ["Go", "BoltDB"],
+    },
+  ];
+
