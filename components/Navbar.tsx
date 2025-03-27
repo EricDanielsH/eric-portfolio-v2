822744b4c1a5e2a62d0daf44453bab4123227c62
Eric Daniels <e.danielshuaman@lancaster.ac.uk>
2025-03-27 15:35:11 +0000

    fix navbar width

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,25 +30,33 @@ export default function Navbar() {
     <nav
       className={`fixed h-[8vh] flex items-center justify-center w-full z-30 transition-colors duration-100 bg-transparent`}
     >
-      <div className="px-6 md:px-14 backdrop-blur w-full max-w-3xl h-full flex items-center justify-between">
-        <Link href="/" className="flex items-end">
-          <Image src="/meow.gif" alt="Cat gif" width={16} height={16} className="w-8 h-8" />
-        </Link>
+      <div className="px-6 md:px-14 backdrop-blur w-full h-full flex items-center justify-center">
+        <div className="px-6 md:px-14  w-full max-w-3xl h-full flex items-center justify-between">
+          <Link href="/" className="flex items-end">
+            <Image
+              src="/meow.gif"
+              alt="Cat gif"
+              width={16}
+              height={16}
+              className="w-8 h-8"
+            />
+          </Link>
 
-        <ul
-          className={`flex items-center justify-center gap-4 transition-transform  duration-300 ease-in-out z-40`}
-        >
-          {links.map((link, index) => (
-            <li
-              key={index}
-              className="my-2 md:my-0 cursor-pointer hover:text-[#ff1717] transition duration-300 font-normal text-gray-700 dark:text-gray-100"
-            >
-              <Link href={link.href}>{link.name}</Link>
-            </li>
-          ))}
-          {/* button for dark mode on medium and bigger devices */}
-          <ThemeSwitcherButton />
-        </ul>
+          <ul
+            className={`flex items-center justify-center gap-4 transition-transform  duration-300 ease-in-out z-40`}
+          >
+            {links.map((link, index) => (
+              <li
+                key={index}
+                className="my-2 md:my-0 cursor-pointer hover:text-[#ff1717] transition duration-300 font-normal text-gray-700 dark:text-gray-100"
+              >
+                <Link href={link.href}>{link.name}</Link>
+              </li>
+            ))}
+            {/* button for dark mode on medium and bigger devices */}
+            <ThemeSwitcherButton />
+          </ul>
+        </div>
       </div>
     </nav>
   );
